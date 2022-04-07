d48f42c76033be8ea48038179c8d3ecc38f47bb5
Olha Gruzglina <olga.zhmura@gmail.com>
2022-04-07 11:53:44 -0400

    commit tree a9e7fa331891bd2562d9ef8d43c8298419f87808

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,13 +1,26 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  if (arr === []) {
+    return -1;
+  }
+  for(let i=0; i < arr.length; i++) {
+      if (arr[i] === arr[i+1]) {
+        return arr[i];
+      }
+  }
+  return -1;
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Expecting: -1");
+  console.log("=>", findFirstDuplicate([]));
+  
+  console.log("\n----------------------------\n");
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
-  console.log("");
+  console.log("\n----------------------------\n");
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
